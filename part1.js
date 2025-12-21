aeb2a41a801773ad3920291b29d567ed5ee80215
surbhisaini123 <shurbhisaini2006@gmail.com>
2025-12-21 20:52:48 +0530

    js

---
+++ part1.js
@@ -0,0 +1,49 @@
+// console.log("hiii")//
+
+// <<<<<<-----TEMPLATE LITERALS--------------->>>>>>>
+
+// let pencilPrice=10
+// let eraserPrice=5
+// let output=`The total price is ${pencilPrice+eraserPrice} rupees.`
+// console.log(output);
+
+// let a=1
+// let b=5
+//<< ----UNINARY OPERATOR---->
+// console.log(a++);
+// console.log(++a);
+
+//<<<<<<---ARTHEMETIC OPERATOR--->>>>>>>
+// console.log(a+b);
+// console.log(a-b);                               
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a^b);
+
+// <<<<------COMPARISION OPERATOR----->>
+//'==' compare value ,not type
+//'===' compare value or type
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+
+
+//<<<<<<----------CONDITIONAL STATMENT------>>>>>
+
+console.log("before my if statrment");
+let age=18
+if(age>20){
+    console.log("you can vote");
+    
+}
+let firstName="surbhi"
+if(firstName=="surbhi"){
+    console.log(`wellcome ${firstName}`);
+    
+}
+
+console.log("after if statement");
+
