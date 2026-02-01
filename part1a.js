4014b774305c80d4707830dc2f1419b9b8061c9f
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-02-01 22:35:02 +0530

    js

--- part1a.js
+++ part1a.js
@@ -13,6 +13,7 @@
 //     console.log("fail")
 // }
 
+
 // ,<<<-------LOGICAL OPERATOR ------>>>
 // AND 
 // let marks=100
@@ -21,8 +22,128 @@
 //     console.log("A++++")
 // }
 // OR
-let marks1=60
-if(marks1>=33 || marks1>=80){
-    console.log("pass")
-    console.log("A++++")
+// let marks1=60
+// if(marks1>=33 || marks1>=80){
+//     console.log("pass")
+//     console.log("A++++")
+// }
+
+//PRACTICE QUESTION 
+// let str='Apple'
+// if(str[0]=='A' && str.length>3){
+//     console.log("GOOD STRING")
+// }else{
+//     console.log("BAD")
+// }
+
+
+//<<<<<<<<<<----------TRUTHLY AND FALSY ------->>>>>>>>>.
+
+// if(null){
+//     console.log("it has true")
+// }else{
+//     console.log("it has false value")
+// }
+// let string='a'
+// if(string){
+//     console.log("string is not empty")
+// }else{
+//     console.log("string is empty")
+// }
+
+
+
+// <<<<<<<<<<-----SWITCH STATEMENT---------->>>>>>>.
+
+// let fruit="apple"
+// switch(fruit){
+//     case 'apple':  console.log("red")
+//     // break
+//     case 'mango': console.log("yellow")
+//     break
+//     case 'grapes': console.log("green")
+//     break
+//     case 'orange': console.log("orange")
+//     break
+//     default: console.log("invali")
+// }
+//PRACTICE QUESTION
+// let day='69'
+// switch(day){
+//     case '1': console.log("sunday")
+//     break
+//     case '2': console.log("monday")
+//     break
+//     case '3': console.log("tuesday")
+//     break
+//     case '4': console.log("wednesday")
+//     break
+//     case '5': console.log("thrusday")
+//     break
+//     case '6': console.log("friday")
+//     break
+//     case '7': console.log("saturday")
+//     break
+//     default: console.log("invaid")
+//     break
+// }
+
+// <<<<------ALERT/PROMPT------->>>>>.
+
+// alert("something is wrong ")
+// alert("danger")
+// console.error("this is error msg")
+// console.warn("this is warning msg")
+
+// let NAME=prompt("enter your name:")
+// console.log(NAME)
+// let ROLL_NO =prompt("enter your ROLL_NO:")
+// console.log(ROLL_NO)
+// let first_name=prompt("enter first_name:")
+// let last_name=prompt("enter your last_name:")
+// // console.log(first_name+ last_name)
+// let msg=first_name+" "+last_name
+// alert(msg)
+
+
+// <<<<<<<<<<<------------ PRACTICE OF PART1------------>>>>>>.
+
+// QUESTION1
+
+// let num=4
+// if(num%10==0){
+//     console.log("good")
+// }else{
+//     console.log("bad")
+// }
+
+// QUESTION2
+
+// let NAME=prompt("enter your name:")
+// let AGE=prompt("enter your age:")
+// // let msg=NAME+" "+"is"+" "+AGE+" "+"year old"
+// let msg=`${NAME} is ${AGE} year old`
+// alert(msg)
+
+// QUESTION3
+// let month=6
+// switch(month){
+//     case 1 : console.log("january,february,march")
+//     break
+//     case 2 : console.log("april,may,june")
+//     break
+//     case 3 : console.log("july,augest,september")
+//     break
+//     case 4 : console.log("octuber,november,december")
+//     break
+//     default: console.log("invalid")
+// }
+
+// QUESTION4
+let num1=34
+let num2=4674
+if(num1%10==num2%10){
+    console.log(num1%10)
+}else{
+    console.log("no mmatch")
 }
