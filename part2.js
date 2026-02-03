37f4c85fa8a63a2233a50d5ded36e5454c46c2ae
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-02-03 21:01:20 +0530

    js

--- part2.js
+++ part2.js
@@ -6,7 +6,12 @@
 // console.log(msg)
 // let newMsg=msg.trim()
 // console.log(newMsg)
-let passWord=prompt("password")
-console.log(passWord)
-let NewPassword=passWord.trim()
-console.log(NewPassword)
+// let passWord=prompt("password")
+// console.log(passWord)
+// let NewPassword=passWord.trim()
+// console.log(NewPassword)
+
+let str='hello'
+console.log(str.toUpperCase())
+let str1='BYYYYYYY'
+console.log(str1.toLowerCase())
