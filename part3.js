8aa56c7db40e3d29a6c4b8f4d19afd99743efe4c
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-02-07 21:39:32 +0530

    js

--- part3.js
+++ part3.js
@@ -46,13 +46,46 @@
 
 //<<<<<<<<-----SMALL TASK------>>>>>>>>>
 
-const favmovie="avtar"
-let guess=prompt("enter my fav movie")
-while((guess!=favmovie)&&(guess!="quit")){
-   guess=prompt("wrong guess try again")
-}
-if(guess==favmovie){
-    console.log("congratulation!!!!!!!! you guess correct movie ")
-}else{
-    console.log("you quit")
+// const favmovie="avtar"
+// let guess=prompt("enter my fav movie")
+// while((guess!=favmovie)&&(guess!="quit")){
+//    guess=prompt("wrong guess try again")
+// }
+// if(guess==favmovie){
+//     console.log("congratulation!!!!!!!! you guess correct movie ")
+// }else{
+//     console.log("you quit")
+// }
+
+// <<<<<<<<<<<--------BRAEK STATEMENT------->>>>>
+// let i=1
+// while(i<=10){
+//     if(i==3){
+//         console.log("use a break at 3")
+//         break
+//     }
+//     console.log(i)
+//     i++
+// }
+
+//  const favmovie="avtar"
+// let guess=prompt("enter my fav movie")
+// while((guess!=favmovie)){
+//     if(guess=="quit"){
+//         console.log("you quit")
+//         break
+//     }
+//    guess=prompt("wrong guess try again")
+// }
+// if(guess==favmovie){
+//     console.log("congratulation!!!!!!!! you guess correct movie ")
+// }
+
+// <<<<<<<<<<----------LOOP WITH ARRAY------>>>>>>>>>>
+let fruit=["apple","mango","banana","litch","orange"]
+// for(let i=0;i<fruit.length;i++){
+//     console.log(i,fruit[i])
+// }
+for(let i=fruit.length-1;i>=0;i--){
+    console.log(i,fruit[i])
 }
