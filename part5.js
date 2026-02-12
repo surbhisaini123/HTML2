d4fdbeb479c67a6e157d5040140efef8cd16c3d5
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-02-12 23:16:14 +0530

    js

--- part5.js
+++ part5.js
@@ -1,4 +1,5 @@
 // <<<,<<<<<<<<<--------------FUNCTION -------->>.>>>>>
+
 // function hello(){         
 //     console.log("hiii")
 // }
@@ -14,7 +15,9 @@
 //     console.log(dice)
 // }
 // dice_roll()
+
 // <<<<<<<<----------FUNTION ARGUMENT--------------.>..>
+
 // function printName(name,age){
 //     console.log(`the name is ${name} and age is ${age}`)
 // }
@@ -24,7 +27,9 @@
 //     console.log(a+b)
 // }
 // sum(2,4)
+
 // <<<<<<<<<<<<<<<<<<-------PRACTICE QUESTION ---------->>>>.>>>..
+
 // function average(a,b,c){
 //     avg=(a+b+c)/3
 //     console.log(avg)
@@ -43,7 +48,9 @@
 
 // table(3)
 
+
 // <<<<,<<<<<--------RETURN KEYWORD--------->>>>>>
+
 // function sum(a,b){
 //     // console.log(a+b)
 //     return a+b
@@ -51,7 +58,9 @@
 // }
 
 // console.log(sum(sum(2,3),3))
+
 // -----SCOPE-------..
+
 //    let sum=33 //global
 
 // function calSum(a,b){
@@ -62,12 +71,15 @@
 // console.log(sum)
 
 // <<<<<<<<<<<-------BLOCK SCOPE---------->>>>>>>>>>
+
 // {
 //     let a=9
 
 // }
 // console.log(a)
+
 // <<<<<<<<<<<<<--------LEXICAL SCOPE -------->>>>>>>>>>>>
+
 // function outerFunc(){
 //     let x=7
 //     let y=5
@@ -84,6 +96,7 @@
 // innerFunc()
 
 // <<<<<<<--------PRACTICE QUESTION------>>>>>>>>>
+
 // let greet="hello" //global scope
 // function outergreet(){
 //     let greet="namaste"  //function scope
@@ -95,6 +108,7 @@
 // }
 // console.log(greet)
 // outergreet()
+
 // <<<<<<<<<<<-----------FUNCTION EXPRESION ----------->>>>>>>>>>>>
 
 // let sum=function(a,b){
@@ -103,6 +117,8 @@
 // console.log(sum(2,3))
 
 // <<<<<<<<<<<<----------HIGHER ORDER FUNCTION && RETURN----------->>>>>>
+
+
 // function multipleGreet(func,count){  //higer order function
 //     for(let i=1;i<=count;i++){
 //         func()
@@ -114,16 +130,63 @@
 // // multipleGreet(greet,10)
 // multipleGreet(function(){console.log("namaste")},10)
 
-const calculator={
-    add: function(a,b){
-        return a+b
-    },
-    multipliation: function(a,b){
-        return a-b
-    },
-    sub: function(a,b){
-        return a-b
+//<<<<<<<<<<<<<<<---------METHOD---------->>.>>>>>>>>> 
+
+// const calculator={
+//     add: function(a,b){
+//         return a+b
+//     },
+//     multipliation: function(a,b){
+//         return a-b
+//     },
+//     sub: function(a,b){
+//         return a-b
+//     }
+// }
+// console.log(calculator)
+// console.log(calculator.add(2,3))
+
+// <<<<<<<<<<<<--------PRACTICE QUESTION------>>>>>>>>>>>>
+
+//  Q1)
+// let arr=[1,2,44,33,7,9]
+
+
+// function larger(num){
+    
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]>num){
+//             console.log( arr[i])
+            
+//         }
+       
+//     }
+
+// }
+// console.log(larger(6))
+// /Q2)
+// let str="abcdabccdefgggh"
+// function extract_unique_char(str){
+//     for(let i=0;i<str.length;i++){
+
+//     }
+
+// }
+// Q4)
+// let str=
+let count=0
+let vowels="aeiou"
+function vowelsToSearch(str){
+    for(let i=0;i<str.length;i++){
+        if(vowels.includes(str[i])){
+        //   return count++
+            // console.log(count)
+              count++
+        }
+        
     }
+    console.log(count)
 }
-console.log(calculator)
-console.log(calculator.add(2,3))
+
+
+vowelsToSearch("ajortuge")
