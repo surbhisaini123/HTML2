6008beaafd057c5a5dd94e3c4aee8b5bb1e82df2
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-02-20 11:18:58 +0530

    js

--- part8.js
+++ part8.js
@@ -1,1 +1,20 @@
-console.dir(document.body)
+// console.dir(document.body)
+// let heading=document.getElementById("heading")
+// console.dir(heading)
+// let element=document.querySelector("p")
+// console.dir(element)
+// let A=document.querySelectorAll("p")
+// console.log(A)
+// let my_class =document.querySelector(".class")
+// console.dir(my_class)
+
+// console.log(element)
+// console.log(element.tagName)
+// console.log(document.querySelector("body").children)
+// let div=document.querySelector("div").innerText
+// let div1=document.querySelector("div").innerHTML
+// console.log(div)
+// console.log(div1)
+// console.log(document.querySelector("div").innerText="abcd")/
+// console.log(document.querySelector("div").innerHTML="<p> inner paharagraph</p>")
+console.log(document.querySelector("div").innerHTML="<i>new heading</i>")
