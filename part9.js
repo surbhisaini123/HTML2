d7608dc7f309cf37ba9ea9593a5e7f54e6428a1c
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-02-23 22:53:03 +0530

    js

--- part9.js
+++ part9.js
@@ -40,22 +40,38 @@
 
 // <<<<<<<<<<<<<<-----ACTIVITY----------------->>>>>>>>>>>>>>>
 
-let btn =document.querySelector("button")
-btn.addEventListener("click",function(){
-    // console.log("generated a random color")
-    let h2=document.querySelector("h2")
-    let randomColor=getRandomcolor()
-    h2.innerText=randomColor
+// let btn =document.querySelector("button")
+// btn.addEventListener("click",function(){
+//     // console.log("generated a random color")
+//     let h2=document.querySelector("h2")
+//     let randomColor=getRandomcolor()
+//     h2.innerText=randomColor
 
-    let div=document.querySelector("div")
-    div.style.backgroundColor = randomColor
-    console.log("color is updated")
+//     let div=document.querySelector("div")
+//     div.style.backgroundColor = randomColor
+//     console.log("color is updated")
+// })
+// function getRandomcolor(){
+//     let red= Math.floor(Math.random()*255)
+//       let green= Math.floor(Math.random()*255)
+//         let blue= Math.floor(Math.random()*255)
+
+//         let color=`rgb(${red},${green},${blue})`
+//         return color
+// // }
+// let paharagraph=document.querySelector("p")
+// paharagraph .addEventListener("click",function(){
+//     console.log("pahagraph was clicked")
+// })
+// let box=document.querySelector(".box")
+// box.addEventListener("mouseenter",function(){
+//     console.log("mouse inside")
+// })
+// <<<<<<<<<<<<<<<<-------"THIS" IN EVENT LISTENER------->>>>>>>
+let paharagraph=document.querySelector("p")
+paharagraph.addEventListener("click",function(){
+    console.log(this.innerText)
+    // console.dir(this)
+    this.style.backgroundColor="blue"
+    this.style.textColor="pink"
 })
-function getRandomcolor(){
-    let red= Math.floor(Math.random()*255)
-      let green= Math.floor(Math.random()*255)
-        let blue= Math.floor(Math.random()*255)
-
-        let color=`rgb(${red},${green},${blue})`
-        return color
-}
