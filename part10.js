4bb6fd86c1bbd6a26051a29562cbcbe6690651a5
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-03-15 23:45:10 +0530

    js

--- part10.js
+++ part10.js
@@ -12,15 +12,92 @@
 // console.log("done,byy")
 
 // <<<<-----visulization call stack------>>>>>>>>>
-function one(){
-    return 1
-}
-function two(){
-    return one()+one()
-}
-function three(){
-    let ans=two()+one()
-    console.log(ans)
-}
-three()
+// function one(){
+//     return 1
+// }
+// function two(){
+//     return one()+one()
+// }
+// function three(){
+//     let ans=two()+one()
+//     console.log(ans)
+// }
+// three()
 // <<<<<<<<<<<<--------BREAKPOINT------->>>>>>>>>>
+// <<<<<<<<<<<<<---------SINGLE THREAD-------------->>>>>>>>>
+//<<<<<<<<---------CALLBACK HELL-------->>>
+
+// h1=document.querySelector("h1")
+// function changeColor(color,delay,nextColorChange){
+//     setTimeout(()=>{
+//    h1.style.color=color
+//    if(nextColorChange)
+//     nextColorChange()
+// },delay)
+
+// }
+// changeColor("red",1000,()=>{
+//     changeColor("orange",1000,()=>{
+//         changeColor("blue",1000,()=>{
+//             changeColor("green",1000,()=>{
+//                 changeColor("pink",1000,()=>{
+//                     changeColor("grey",1000)
+//                 })
+//             })
+//         })
+//     })
+
+// })
+
+// changeColor("red",1000)
+// changeColor("orange",2000)
+// changeColor("blue",3000)
+
+// /<<<<<<<<<<<<------PROISE----------.>>>>>>>>>>>>>
+// 1st method
+// function savetoDb(data){
+//     let internetSpeed=Math.floor(Math.random() * 10)+1
+//     if(internetSpeed > 4){
+//         console.log("your data was saved:",data)
+//     }else{
+//         console.log('weak onnetion')
+//     }
+// }
+// 2nd method
+function savetoDb(data,succes,failure){
+    let internetSpeed=Math.floor(Math.random() * 10)+1
+    if(internetSpeed > 4){
+        succes()
+    }else{
+        failure()
+    }
+}
+savetoDb(
+    "hello",
+    ()=>{
+         console.log(" success :your data was saved")
+         savetoDb(
+            "surbhi",
+            ()=>{
+                console.log("succes2 data2 was saved")
+                savetoDb(
+                    'khushi',
+                    ()=>{
+                        console.log("succes3 data was saved")
+                    },
+                    ()=>{
+                        console.log("failure3 weak onnection")
+                    }
+                )
+
+            },
+            ()=>{
+                console.log("failure2 weak connection")
+            }
+         )
+        },
+    ()=>{
+        console.log('failure:weak onnetion')
+
+    }
+)
