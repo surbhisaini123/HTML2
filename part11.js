f6b97903c5a71297cdc7df6e01a61904a30fe6a2
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-03-25 15:50:48 +0530

    js

--- part11.js
+++ part11.js
@@ -19,23 +19,59 @@
 //     return 5
 // }
 
-// AWAIT KEYWORD
+// <<<<<<<<<----------AWAIT KEYWORD---------->>>>>>>>>>>
 
-function getNum(){
+// function getNum(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//               let num= Math.floor(Math.random()*10)+1
+//         console.log(num)
+//         resolve()
+
+//         },1000)
+      
+//     })
+// }
+
+// async function demo(){
+//     await getNum()
+//      await getNum()
+//       await getNum()
+//        getNum()
+// }
+
+h1=document.querySelector("h1")
+
+function changeColor(color,delay){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-              let num= Math.floor(Math.random()*10)+1
-        console.log(num)
-        resolve()
-
-        },1000)
-      
+            let num = Math.floor(Math.random()*5)+1
+            if(num>3){
+                reject("promise is reject")
+            }
+            h1.style.color=color
+            console.log(`color change to ${color}!`)
+            resolve("color changed")
+        },delay)
     })
+    
 }
 
 async function demo(){
-    await getNum()
-     await getNum()
-      await getNum()
-       getNum()
+    try{
+        await changeColor("red",1000)
+    await changeColor("blue",1000)
+    await changeColor("green",1000)
+   await changeColor("pink",1000)
+
+    }catch(err){
+        console.log("error is caught")
+        console.log(err)
+    }
+    
+
+   let a=3
+   console.log("new number is:",a+3)
+
 }
+
